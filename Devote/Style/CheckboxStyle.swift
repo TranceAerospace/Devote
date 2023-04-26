e9dff9e34126593f7211b4d6391e34e44de6a9a2
Trance <xp46290@gmail.com>
2023-04-26 16:47:32 -0400

    Custom List Item Row with a checkbox

---
+++ Devote/Style/CheckboxStyle.swift
@@ -0,0 +1,32 @@
+//
+//  CheckboxStyle.swift
+//  Devote
+//
+//  Created by Mark Trance on 4/25/23.
+//
+
+import SwiftUI
+
+struct CheckboxStyle: ToggleStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        return HStack {
+            Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+                .foregroundColor(configuration.isOn ? .pink : .primary)
+                .font(.system(size: 30, weight: .semibold, design: .rounded))
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            configuration.label
+        } //: HStack
+    }
+}
+
+struct CheckboxStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Toggle("Placeholder label", isOn: .constant(true))
+            .toggleStyle(CheckboxStyle())
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
